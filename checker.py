fc65f5729dec5f004bd84b72958cc647d26a0a26
nalivayev <nalivayev@gmail.com>
2022-02-24 10:17:19 +0300

    first version

--- checker.py
+++ checker.py
@@ -1,25 +1,95 @@
+from argparse import ArgumentParser
+from typing import Any
 from PIL import Image
+from os import path, listdir, rename
+
+import logging
 
 
-class CheckerParser:
+class CheckerParser(ArgumentParser):
 
-    pass
+    __required_group: Any
+
+    def __init__(self):
+        ArgumentParser.__init__(self)
+        self.__required_group = self.add_argument_group("required arguments")
+        self.__required_group.add_argument("-p", "--path", type=str, help="path to check", required=True, default=None)
+        self.add_argument('-r', '--recursively', action='store_false', help='including subdirectories')
+
+    def parse_args(self, args=None, namespace=None):
+        v_result = ArgumentParser.parse_args(self, args, namespace)
+        if not path.isdir(v_result.path):
+            self.error('Source path not found')
+        return v_result
 
 
 class CheckerLogger:
 
-    pass
+    __logger: logging.Logger
+
+    def __init__(self):
+        self.__logger = logging.getLogger()
+        self.__logger.setLevel(logging.INFO)
+        v_formatter = logging.Formatter("%(asctime)s.%(msecs)03d %(message)s", datefmt="%Y.%m.%d %H:%M:%S")
+        v_handler = logging.FileHandler(f"{path.splitext(__file__)[0]}.log", mode="w")
+        v_handler.setFormatter(v_formatter)
+        self.__logger.addHandler(v_handler)
+        v_handler = logging.StreamHandler()
+        v_handler.setFormatter(v_formatter)
+        self.__logger.addHandler(v_handler)
+
+    def info(self, p_message: str) -> None:
+        self.__logger.info(p_message)
 
 
-class Checker:
+class FileChecker:
 
     def do(self, p_file_name: str):
+        v_result = True
         try:
-            im = Image.load(p_file_name)
-            im.verify()
-            im.close()
-            im = Image.load(p_file_name)
-            im.transpose(Image.FLIP_LEFT_RIGHT)
-            im.close()
+            v_image = Image.open(p_file_name)
+            v_image.verify()
+            v_image.close()
+            v_image = Image.open(p_file_name)
+            v_image.transpose(Image.FLIP_LEFT_RIGHT)
+            v_image.close()
         except Exception:
-            pass
+            v_result = False
+        return v_result
+
+
+class PathChecker:
+
+    __logger: CheckerLogger
+    __checker: FileChecker
+
+    def __add_log_message(self, p_message):
+        if self.__logger is not None:
+            self.__logger.info(p_message)
+
+    def __do_recursively(self, p_path, p_recursively):
+        v_list = listdir(p_path)
+        while len(v_list) > 0:
+            v_path = path.join(p_path, v_list[0])
+            v_list.pop(0)
+            if path.isfile(v_path):
+                if not self.__checker.do(v_path):
+                    v_name, v_extension = path.splitext(v_path)
+                    try:
+                        self.__add_log_message(f"File {v_path} is corrupt")
+                        v_name = f"{v_name}.corrupt{v_extension}"
+                        rename(v_path, v_name)
+                    except Exception:
+                        self.__add_log_message(f"Error rename file {v_list[0]}")
+                    else:
+                        self.__add_log_message(f"File {v_path} renamed to {v_name}")
+                else:
+                    self.__add_log_message(f"File {v_path} is good")
+            else:
+                if p_recursively and path.isdir(v_path):
+                    self.__do_recursively(v_path, p_recursively)
+
+    def do(self, p_logger: CheckerLogger, p_path, p_recursively):
+        self.__logger = p_logger
+        self.__checker = FileChecker()
+        self.__do_recursively(path.abspath(p_path), p_recursively)
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from checker import Checker, CheckerParser, CheckerLogger
+from checker import PathChecker, CheckerParser, CheckerLogger
 
 
 def main():
     v_parser = CheckerParser()
     v_args = v_parser.parse_args()
     v_logger = CheckerLogger()
-    v_marauder = MultiMarauder()
+    v_checker = PathChecker()
     try:
-        v_marauder.do(v_logger, v_args.id, v_args.skip, v_args.count, v_args.groupcount, v_args.flowcount)
+        v_checker.do(v_logger, v_args.path, v_args.recursively)
     except Exception:
         v_logger.info(f"Unknown error")
 
